85df13a352a98ee1d68eeea8de969a02edf97489
Ronaldo Junior <ronaldosenajr@gmail.com>
2021-06-23 17:33:34 -0400

    Realizado o programa 3

--- exercises/bloco-4/dia-1/Exercicio-2&3.js
+++ exercises/bloco-4/dia-1/Exercicio-2&3.js
@@ -7,3 +7,16 @@ if (num1 > num2){
 else{
     console.log(num2);
 }
+
+//Programa 3
+let number1 = 5;
+let number2 = 4;
+let number3 = 2;
+
+if (number1 > number2 && number1 > number3){
+    console.log(number1);
+}else if (number2 > number1 && number2 > number3){
+    console.log(number2);
+}else{
+    console.log(number3);
+}
