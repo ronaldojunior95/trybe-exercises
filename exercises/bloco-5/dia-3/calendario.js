8910fa1531b8f583aa76adb1a32e0b91fdb5964e
Ronaldo Junior <ronaldosenajr@gmail.com>
2021-07-06 20:57:52 -0400

    Realizado o exercicio 1

---
+++ exercises/bloco-5/dia-3/calendario.js
@@ -0,0 +1,50 @@
+function createDaysOfTheWeek() {
+    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+    const weekDaysList = document.querySelector('.week-days');
+  
+    for (let index = 0; index < weekDays.length; index += 1) {
+      const days = weekDays[index];
+      const dayListItem = document.createElement('li');
+      dayListItem.innerHTML = days;
+  
+      weekDaysList.appendChild(dayListItem);
+    };
+  };
+  
+  createDaysOfTheWeek();
+  
+  // Escreva seu código abaixo.
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
+
+  function addDaysToMonth(){
+    const days = document.querySelector('#days');
+
+    for (let i = 0; i < dezDaysList.length; i += 1){
+        let day = document.createElement('li');
+        day.innerHTML = dezDaysList[i];
+        day.classList = 'days';
+
+        //Adiciona classes dependendo do dia.
+        if (day.innerHTML === '24' || day.innerHTML === '31'){
+            console.log(day);
+            day.classList.add('holiday');
+        } else if (day.innerHTML === '4' || day.innerHTML === '11' || day.innerHTML === '18'){
+            console.log(day);
+            day.classList.add('friday');
+        } else if (day.innerHTML === '25'){
+            console.log(day);
+            day.classList.add('holiday');
+            day.classList.add('friday');
+        }
+        
+        days.appendChild(day);
+    }
+    
+    //remove os dias de novembro.
+    for (let i = 0; i < 2; i += 1){
+        let day = days.children[i];
+        day.innerHTML = ''
+    }
+  }
+
+  addDaysToMonth();
