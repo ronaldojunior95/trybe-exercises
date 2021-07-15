60bdb4e67e989fe53a06a8eb19cfa2f114a14b3a
Ronaldo Junior <ronaldosenajr@gmail.com>
2021-07-15 17:26:24 -0400

    Realizado a estruturação em html

---
+++ exercises/bloco-6/dia-1/script.js
@@ -0,0 +1,16 @@
+let enviarBtn = document.querySelector('#enviar-btn');
+let apagarBtn = document.querySelector('#apagar-btn');
+let estadosHolder = document.querySelector('#estado-input')
+
+enviarBtn.addEventListener('click', sendData);
+
+
+
+
+
+
+
+
+function sendData(event) {
+	event.preventDefault();
+}
