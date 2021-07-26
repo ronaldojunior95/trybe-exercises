2819662173063f715ba67c6d9ffdf7038cb71725
Ronaldo Junior <ronaldosenajr@gmail.com>
2021-07-26 16:42:39 -0400

    Realizado o ex 2 da pt2

--- exercises/bloco-7/dia-1/pt2.js
+++ exercises/bloco-7/dia-1/pt2.js
@@ -11,9 +11,16 @@ const factorial = (number) => {
 	return result;
 };
 
-console.log(factorial(5));
-
 //Com Recursividade
 const facto = number => number > 1 ? number * facto(number - 1) : 1;
 
-console.log(facto(5));
+//Ex 2.
+const longestWord = str => {
+	const longestWord = str.split(' ').sort(function(a ,b) {
+		return b.length - a.length;
+	})
+	console.log(`${longestWord[0]}: ${longestWord[0].length}`);
+	return longestWord[0].length;
+}
+
+longestWord("Antônio foi no banheiro e não sabemos o que aconteceu");
