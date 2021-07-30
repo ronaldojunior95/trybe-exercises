89dc0ecf9209ed76aee07fcfc52ac36781f4815c
Ronaldo Junior <ronaldosenajr@gmail.com>
2021-07-30 18:12:20 -0400

    Realizado o ex 3

--- exercises/bloco-8/dia-2/exercise1.js
+++ exercises/bloco-8/dia-2/exercise1.js
@@ -84,3 +84,21 @@ function smallerName() {
 }
 
 assert.strictEqual(smallerName(), 'Duna');
+
+//Ex 3
+const expectedResult = {
+	author: {
+		birthYear: 1948,
+		name: 'George R. R. Martin',
+	},
+	genre: 'Fantasia',
+	id: 1,
+	name: 'As Crônicas de Gelo e Fogo',
+	releaseYear: 1991,
+};
+
+function getNamedBook() {
+	return books.find((book) => book.name.length === 26);
+}
+
+assert.deepStrictEqual(getNamedBook(), expectedResult);
