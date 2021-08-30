6bd718232eb8e4d3507745ef437f9725de0b1f81
Ronaldo Junior <ronaldosenajr@gmail.com>
2021-08-30 18:19:32 -0400

    Realizado os 2 primeiros exercicios

--- exercises/front-end/bloco-11/dia-1/my-first-app/src/App.js
+++ exercises/front-end/bloco-11/dia-1/my-first-app/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+
+const Task = (value) => {
+	return (
+		<li>{value}</li>
+	)
+}
+
+const tarefas = ['Comer', 'Dormir', 'Codar', 'Jogar'];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <ol>
+	  {tarefas.map((tarefa) => Task(tarefa))}
+  </ol>
   );
 }
 
